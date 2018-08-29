2bbe2007d660eb5e7214edefb3fd0fbf08dc895e
Joe Cridge <joe.cridge@me.com>
2018-08-29 02:24:09 +0100

    Move Hammerspoon implementation into new repo

---
+++ hammerspoon/README.md
@@ -0,0 +1,75 @@
+# Jeo Keyboard Layout for Hammerspoon
+
+This is an implementation of the [Jeo Keyboard Layout][JKL] for macOS Sierra
+(and onwards) using [Hammerspoon](http://www.hammerspoon.org/).
+
+![Layout diagram](https://user-images.githubusercontent.com/11491479/27187883-4a3be07a-51e4-11e7-82dd-ad47b31019ca.png)
+
+## Installation
+
+1.  Install Hammerspoon (`brew cask install hammerspoon`) and configure it to
+    launch at login.
+
+2.  Stop macOS listening to Caps Lock by opening System Preferences, choosing
+    Keyboard and clicking the “Modifier Keys...” button. Change Caps Lock to
+    “No Action” and click OK.
+
+    _Do this for your internal _and_ external keyboard_.
+
+3.  Install [Karabiner-Elements](https://github.com/tekezo/Karabiner-Elements)
+    (`brew cask install karabiner-elements`) and add the following ‘simple
+    modifications’:
+
+    | From key    | To key                                |
+    | ----------- | ------------------------------------- |
+    | caps_lock   | quote key (')                         |
+    | application | right_alt (equal to \`right_option\`) |
+    | f14         | f16                                   |
+    | f15         | f17                                   |
+
+    Additionally, make the following changes in the Function Keys tab:
+
+    | From key | To key |
+    | -------- | ------ |
+    | f5       | f5     |
+    | f6       | f6     |
+
+4.  Download and install Jeo:
+
+    ```sh
+     cd ~/.hammerspoon/
+     git clone https://github.com/joecridge/jeospoon.git
+    ```
+
+5.  Add the following lines to your `~/.hammerspoon/init.lua`:
+
+    ```lua
+    jeo = require 'jeospoon'
+    jeoEventTap = hs.eventtap.new(jeo.KEY_EVENTS, jeo.handleKeyEvent)
+    jeoEventTap:start()
+    ```
+
+6.  Reload Hammerspoon.
+
+## Configuration
+
+You can set the editor and terminal to launch as follows:
+
+```lua
+jeo.setEditor('TextEdit')
+jeo.setTerminal('Terminal')
+```
+
+(The values shown above are the defaults.)
+
+## Pass-Through Mode
+
+You can toggle pass-through mode (where this remapping is completely disabled)
+by pressing ⌘⎋ (`Cmd-Esc`), or programmatically within hammerspoon:
+
+```lua
+jeo.enablePassThrough()  -- Keyboard is now standard QWERTY.
+jeo.disablePassThrough() -- Keyboard is now Jeo remapped.
+```
+
+[JKL]: https://github.com/joecridge/jeo
---
+++ hammerspoon/numpad.lua
@@ -0,0 +1,45 @@
+return {
+  ['1'] = { key = 'none', alt = false, shift = false },
+  ['2'] = { key = 'none', alt = false, shift = false },
+  ['3'] = { key = 'none', alt = false, shift = false },
+  ['4'] = { key = 'none', alt = false, shift = false },
+  ['5'] = { key = 'none', alt = false, shift = false },
+  ['6'] = { key = '3',    alt = false, shift = true  },
+  ['7'] = { key = 'tab',  alt = false, shift = false },
+  ['8'] = { key = 'pad/', alt = false, shift = false },
+  ['9'] = { key = 'pad*', alt = false, shift = false },
+  ['0'] = { key = 'pad-', alt = false, shift = false },
+
+  ['q'] = { key = 'pageup',        alt = false, shift = false },
+  ['w'] = { key = 'delete',        alt = false, shift = false },
+  ['e'] = { key = 'up',            alt = false, shift = false },
+  ['r'] = { key = 'forwarddelete', alt = false, shift = false },
+  ['t'] = { key = 'pagedown',      alt = false, shift = false },
+  ['y'] = { key = 'none',          alt = false, shift = false },
+  ['u'] = { key = 'pad7',          alt = false, shift = false },
+  ['i'] = { key = 'pad8',          alt = false, shift = false },
+  ['o'] = { key = 'pad9',          alt = false, shift = false },
+  ['p'] = { key = 'pad+',          alt = false, shift = false },
+
+  ['a'] = { key = 'home',  alt = false, shift = false },
+  ['s'] = { key = 'left',  alt = false, shift = false },
+  ['d'] = { key = 'down',  alt = false, shift = false },
+  ['f'] = { key = 'right', alt = false, shift = false },
+  ['g'] = { key = 'end',   alt = false, shift = false },
+  ['h'] = { key = ',',     alt = false, shift = false },
+  ['j'] = { key = 'pad4',  alt = false, shift = false },
+  ['k'] = { key = 'pad5',  alt = false, shift = false },
+  ['l'] = { key = 'pad6',  alt = false, shift = false },
+  [';'] = { key = ';',     alt = false, shift = true  },
+
+  ['z'] = { key = 'escape',   alt = false, shift = false },
+  ['x'] = { key = 'tab',      alt = false, shift = false },
+  ['c'] = { key = 'none',     alt = false, shift = false }, -- TODO: insert.
+  ['v'] = { key = 'padenter', alt = false, shift = false },
+  ['b'] = { key = 'none',     alt = false, shift = false }, -- TODO: undo.
+  ['n'] = { key = 'pad.',     alt = false, shift = false },
+  ['m'] = { key = 'pad1',     alt = false, shift = false },
+  [','] = { key = 'pad2',     alt = false, shift = false },
+  ['.'] = { key = 'pad3',     alt = false, shift = false },
+  ['/'] = { key = ';',        alt = false, shift = false }
+}
